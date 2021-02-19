0fe82a4ec1211461d2e5902d02aac2b22225b918
lkeme <1462364497@qq.com>
2021-02-19 20:59:33 +0800

    [add] Auth,Logger

--- app/server.py
+++ app/server.py
@@ -7,21 +7,58 @@
 - time: 2021/2/19 11:26
 - desc:
 """
-import json
 from gevent import monkey
 
 monkey.patch_all()
-
+import os
+import re
+import json
 from gevent.pywsgi import WSGIServer
-from app.client import Client
 from flask import Flask, abort, jsonify, request, send_from_directory
+from app.client import Client
+from app.config import Config
+import termcolor
+import traceback
+import logging
+from logging.handlers import TimedRotatingFileHandler
 
 app = Flask(__name__)
 
 
-def main(address="0.0.0.0", port=5000):
-    http_server = WSGIServer((address, port), app)
-    http_server.serve_forever()
+# 处理 Flask 写日志到文件带有颜色控制符的问题
+def colored(text, color=None, on_color=None, attrs=None):
+    who_invoked = traceback.extract_stack()[-2][2]  # 函数调用人
+    if who_invoked == 'log_request':
+        # 如果是来自 Flask/werkzeug 的调用
+        return text
+    else:
+        # 来自其他的调用正常高亮
+        COLORS = termcolor.COLORS
+        HIGHLIGHTS = termcolor.HIGHLIGHTS
+        ATTRIBUTES = termcolor.ATTRIBUTES
+        RESET = termcolor.RESET
+        if os.getenv('ANSI_COLORS_DISABLED') is None:
+            fmt_str = '\033[%dm%s'
+            if color is not None:
+                text = fmt_str % (COLORS[color], text)
+            if on_color is not None:
+                text = fmt_str % (HIGHLIGHTS[on_color], text)
+            if attrs is not None:
+                for attr in attrs:
+                    text = fmt_str % (ATTRIBUTES[attr], text)
+            text += RESET
+        return text
+
+
+@app.before_request
+def process():
+    config = Config()
+    # 如果没开启验证，直接跳过
+    if config.get('auth', 'enable'):
+        token = request.args.get("token")
+        # 如果不符合条件，返回40
+        if token != config.get('auth', 'token'):
+            abort(401)
 
 
 @app.route("/")
@@ -50,6 +87,29 @@ def fetch_mail():
 
     result = Client(email).query_mail()
     return jsonify(result)
+
+
+def main(address="0.0.0.0", port=5000):
+    # 日志处理
+    logger = logging.getLogger('GmailAPI')
+    logging.basicConfig(level=logging.INFO)  # 记录访问
+    web_log_path = os.path.join(os.getcwd(), 'logs', 'web.log')
+    handler = TimedRotatingFileHandler(filename=web_log_path, when='midnight',
+                                       backupCount=7, encoding='utf-8')
+    handler.suffix = '%Y-%m-%d.log'
+    handler.extMatch = re.compile(r'^\d{4}-\d{2}-\d{2}.log')
+    # formatter = logging.Formatter(
+    #     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.propagate = False  # 不在控制台上输出
+
+    termcolor.colored = colored
+    app.logger.addHandler(handler)
+    # 日志处理
+
+    http_server = WSGIServer((address, port), app, log=app.logger)
+    http_server.serve_forever()
 
 
 if __name__ == "__main__":
--- example/api.py
+++ example/api.py
@@ -1,10 +1,9 @@
 import json
-
 import requests
 
 
-def test():
-    url = 'http://127.0.0.1:5000/fetch_mail'
+def test_api(token):
+    url = f'http://127.0.0.1:5000/fetch_mail?token={token}'
     data = {
         'email': 'example@gmail.com'
     }
@@ -13,9 +12,9 @@ def test():
     }
     # {'code': 404, 'data': {}, 'msg': 'no_data'}
     response = requests.post(url, headers=headers, json=data).json()
-    print(json.dumps(response, sort_keys=True, indent=2,ensure_ascii=False))
+    print(json.dumps(response, sort_keys=True, indent=2, ensure_ascii=False))
     return response
 
 
 if __name__ == '__main__':
-    test()
+    test_api("92cb25e61c6c6bfa")
